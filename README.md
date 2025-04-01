3d29f7999aad9282622555f5baca6c99c0ffb0a3
Lucas <lucajohnst@gmail.com>
2025-04-01 12:23:29 -0700

    Some hw 6 progress

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Class related files for a certain class taught by Dr. S. The class and professor
 
 To run a homework problem, first, `cd` into the relevant directory or navigate with MATLAB to set the right homework folder as your working directory. Then run the homework problems as standalone MATLAB script. For some of these \*.m files, you might need to add the files in the `scripts` directory to your MATLAB path (run `help path` in MATLAB for more information).
 
-Each homework file is well-commented, and should include references to various tables used to justiy values, and a clear description of the units of each variable if applicable.
+Each homework file is well-commented, and should include references to various tables used to justify values, and a clear description of the units of each variable if applicable.
 
 Homework scripts are not intended to be be complete on their own, and often reference pen-and-paper work that describes how various quantities are measured or defined. They are primarily used to ensure calculations are done reliably, and are easy to adjust should I notice an error in my process.
 
---
+++ hw6/problem1.m
@@ -0,0 +1,167 @@
+clear all;
+
+% NOTE: Shigley table numbers, figure numbers are with respect to the Tenth edition
+%   This means there's a discrepancy with the slides where Table 6-4 on the slides (reliability
+%   factor) is Table 6-5 in the Tenth Edition
+
+
+% Notation:
+%   I may use mat to abreviate material
+%   _st is the AISI 1020 CD Steel
+%   _al is the 319-T6 Aluminum
+
+%% Geometry
+b = 2.5*10; % [mm]
+h = 0.5*10; % [mm]
+
+
+%% Material Properties
+% First things first--transform ultimate strength to operating temperature
+% Operating temperature
+T_op = 150; % [C]
+
+% Use formula given in slides for temperature in Celcius
+S_T_by_S_RT = 0.99 + 5.9*10^(-4)*T_op - 2.1 * 10^(-6)*T_op^2;
+% Sanity check manually with Table 6-4 values
+
+% Table A-20 in Shigley, transformed for temp
+S_ut_st = 470 * S_T_by_S_RT; % [MPa]
+% Table A-24
+S_ut_al = 248 * S_T_by_S_RT; % [MPa]
+
+% Assume high cycle fatigue because, uhm, uh, hmmm
+%   Engineering decision
+
+%% Perfect Endurance limits/fatigue strength at 5E8 cycles
+% Table A-24.b
+S_f_5E8_prime_al = 69; % [MPa]
+S_e_prime_st = NaN; % [MPa]
+
+% Eq 6-10
+if (S_ut_st <= 1400) % 1400 is in [MPa]
+    S_e_prime_st = 0.5*S_ut_st;
+elseif (S_ut_al > 1400) % 1400 is in [MPa]
+    S_e_prime_st = 700; % [MPa]
+else 
+    assert(false, "You've messed something up, you're on your own on this one.");
+end
+
+
+fprintf("S_e_prime_st      = %6.3f MPa.\n", S_e_prime_st);
+fprintf("S_f_5E8_prime_al  = %7.3f MPa.\n\n", S_f_5E8_prime_al);
+
+% Now that we have our endurance limits/fatigue strength at 5E8, need Marin factors
+
+%% k_a 
+
+% Use Table 6-2
+
+% Cold Drawn, in MPa
+a_st = 3.04;
+b_st = -0.217;
+
+% Table A-24.b lists this aluminum alloy as cast (forged), in MPa
+a_al = 54.9;
+b_al = -0.758;
+
+k_a_al = a_st*S_ut_al^b_al;
+k_a_st = a_st*S_ut_st^b_st;
+
+fprintf("k_a_st = %.3f \t k_a_al = %.3f\n", k_a_st, k_a_al);
+
+%% k_b
+
+% Same geometry/loading for both => same k_b;
+
+% Axial loading
+k_b_al = 1;
+k_b_st = 1;
+
+fprintf("k_b_st = %.3f \t k_b_al = %.3f\n", k_b_st, k_b_al);
+
+%% k_c 
+
+% Both are pure axial
+k_c_al = 0.85;
+k_c_st = 0.85;
+
+fprintf("k_c_st = %.3f \t k_c_al = %.3f\n", k_c_st, k_c_al);
+
+%% k_d 
+
+% Already transformed temperature earlier
+k_d_al = 1;
+k_d_st = 1;
+
+fprintf("k_d_st = %.3f \t k_d_al = %.3f\n", k_d_st, k_d_al);
+
+%% k_e 
+
+% We're told 97% reliability, but that's not listed in Table 6-4. Using my Engineering Intuition,
+%   I'm just going to linearly interpolate
+k_e_al = interp1([95, 99], [0.868, 0.814], 97);
+k_e_st = k_e_al;
+
+fprintf("k_e_st = %.3f \t k_e_al = %.3f\n", k_e_st, k_e_al);
+
+%% k_f
+
+% No miscellanous effects are present
+k_f_al = 1;
+k_f_st = 1;
+
+fprintf("k_f_st = %.3f \t k_f_al = %.3f\n", k_f_st, k_f_al);
+
+
+%% Real Endurance limits/fatigue strength at 5E8 cycles
+
+S_e_st = k_a_st * k_b_st * k_c_st * k_d_st * k_e_st * k_f_st * S_e_prime_st;
+S_f_5E8_al = k_a_al * k_b_al * k_c_al * k_d_al * k_e_al * k_f_al * S_f_5E8_prime_al;
+
+fprintf("\nS_e_st      = %6.3f MPa.\n", S_e_st);
+fprintf("S_f_5E8_al  = %7.3f MPa.\n", S_f_5E8_al);
+
+% Whoops, ignore everything after this line
+
+% %% Calculate cycle lifetime
+% 
+% % Aluminum
+% N_max_al = 5E8;
+% 
+% % See Figure 6-18
+% % Reading the text, this is applicable for when S_e = 0.5*S_y 
+% %   However, we're going to have to make some CRUDE approximations here as I don't see an 
+% %   applicable chart for aluminum
+% % use temperature-transformed S_ut value
+% % 255.7 MPa ~ 37.1 ksi, which is off the chart, therefore use f = 0.9
+% f_al = 0.9;
+% 
+% % Need to be careful, Shigley, slides treat log as layman/engineers (log_10), but MATLAB
+% %   treats it as a mathmatician might (natural log), need to be explicit
+% % Personally, log being log_10 makes more sense, because ln is shorter anyways
+% b = 1/(3 - log10(N_max_al)) * log10(f_al * S_ut_al / S_f_5E8_al);
+% a = 10^((log10(N_max_al) * log10(f_al * S_ut_al) - 3*log10(S_f_5E8_al))/(log10(N_max_al)-3));
+% 
+% % Steel
+% % Figure 6-18, use temperature-transformed S_ut value
+% % "If you squint, it's just slightly less than 0.9..."
+% f_st = 0.899;
+% 
+% a_st = (f_st * S_ut_st)^2 / (S_e_st);
+% b_st = -1/3 * log10(f_st*S_ut_st / S_e_st);
+% 
+% N_al = (S_f_5E8_al/a_al) ^ (1/b_al); % [cycles]
+% N_st = (S_e_st/a_st) ^ (1/b_st); % [cycles]
+% 
+% fprintf("\nN_st  = %s cycles.\n", addComma(N_al));
+% fprintf(  "N_al  = %s cycles.\n", addComma(N_st));
+% 
+% % Use Java formatting code to get nicer looking numbers
+% % MATLAB is kind of secretly Java, some C++, and probably a little left over FORTRAN in a coat
+% %   anyways
+% % https://www.mathworks.com/matlabcentral/answers/96131-is-there-a-format-in-matlab-to-display-numbers-such-that-commas-are-automatically-inserted-into-the
+% function numOut = addComma(numIn)
+%    import java.text.*
+%    jf=java.text.DecimalFormat; % comma for thousands, three decimal places
+%    numOut= char(jf.format(numIn)); % omit "char" if you want a string out
+% end
